6fb13f2cb9097a2adc7aebc482292d7a7b2c1e6d
Shigeki Suwa <suwa.shigeki@gmail.com>
2018-05-15 15:12:02 +0900

    update

--- src/disassembler.hpp
+++ src/disassembler.hpp
@@ -28,17 +28,14 @@ private:
   struct exec header;
 
   void analyze_text(const char text[], const size_t len);
-  // unsigned char text_mov(const char *word);
-  // unsigned char text_int(const char *word);
-  // unsigned char text_add(const char *word);
   size_t mov_3(const char *head);
   size_t add_1(const char *head);
   size_t int_1(const char *head);
   string get_reg_name(const struct info &);
   string get_rm_string(const struct info &);
-  void print_line_number(const size_t);
   void print_data_wide(const unsigned short data);
   void print_data_narrow(const unsigned char data);
+  void print_line_number(const size_t);
   void print_bytes(const char *head, const size_t len);
   void print_byte(const char);
 };
